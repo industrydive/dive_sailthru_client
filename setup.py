821219d50a544a9d5c922fff87c8fd84b2a4c07b
David Barbarisi <davidbarbarisi@gmail.com>
2015-11-06 16:43:05 -0500

    Add certifi version lock and some metadata.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "dive_sailthru_client",
     version = "0.0.1",
+    description="Industry Dive abstraction of the Sailthru API client",
+    author='David Barbarisi',
+    author_email='dbarbarisi@industrydive.com',
+    url='https://github.com/industrydive/dive_sailthru_client',
+    license='Python',
     zip_safe=False,
     packages=['dive_sailthru_client'],
     install_requires=[
         'sailthru-client==2.2.0',
+        'certifi==2015.04.28'
     ],
     test_suite='nose.collector',
     tests_require=['nose', 'mock']
