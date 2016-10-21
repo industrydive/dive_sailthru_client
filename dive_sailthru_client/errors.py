b353070621ff704e05be0253e6476a07a34528ca
Miriam Sexton <miriam@industrydive.com>
2016-10-21 11:43:37 -0400

    Actually commit SailthruApiError fully so it works :)

--- dive_sailthru_client/errors.py
+++ dive_sailthru_client/errors.py
@@ -9,12 +9,11 @@ class SailthruApiError(SailthruClientError):
         catch this too.
     """
     # TODO: add accessor for response_error object
-    pass
 
     def __init__(self, *args, **kwargs):
-        super(SailthruApiError, self).__init__(*args, **kwargs)
+        self.api_error_code = kwargs.pop('api_error_code', 0)
+        self.api_error_message = kwargs.pop('api_error_message', '')
 
-        self.api_error_code = kwargs.get('api_error_code', 0)
-        self.api_error_message = kwargs.get('api_error_message', '')
+        super(SailthruApiError, self).__init__(*args, **kwargs)
 
 
