66e6ee12605be93c6d9a8b4f82275bb917b2e2b2
Eli Dickinson <eli@industrydive.com>
2017-05-24 09:16:14 -0400

    update version string

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dive_sailthru_client",
-    version="0.0.10",
+    version="0.0.11-DEV",
     description="Industry Dive abstraction of the Sailthru API client",
     author='David Barbarisi',
     author_email='dbarbarisi@industrydive.com',
