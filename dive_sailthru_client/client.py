9b26bcedff13c308668ab3419a8bf6f849f50e74
llorenz <llorenz@industrydive.com>
2017-10-02 14:01:01 -0400

    do the same to the name field that we did to the subject field. This is an attempt to get later string functions (like ''.join()), specifically inside the json.dumps() function, that appear to operate on arguments of mixed unicode and byte type, some of which will be non-ascii-encodable data (ie, by our empirical observation, something in a campaign's ['name'] field) will not trigger a UnicodeDecodeError.

--- dive_sailthru_client/client.py
+++ dive_sailthru_client/client.py
@@ -190,6 +190,7 @@ class DiveSailthruClient(object):
                 c['dive_brand'] = self._infer_dive_brand(c)
                 # Automatically "fix" unicode problems.
                 # TODO: Not sure this is right.
+                c['name'] = c['name'].encode('utf-8', errors='replace')
                 c['subject'] = c['subject'].encode('utf-8', errors='replace')
                 campaigns.append(c)
 
